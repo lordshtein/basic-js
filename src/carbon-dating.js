53f7e9d2ddbedbad952b6af197f5383055e20f18
lordshtein <lordshtein@gmail.com>
2020-03-10 01:41:34 +0300

    feat: add cats and carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity === "nubmer" || typeof sampleActivity === "string") {
+    let act = parseFloat(sampleActivity)
+    if (act <= 15 && act > 0) {
+      let age = Math.log(MODERN_ACTIVITY / act) / 0.693 * HALF_LIFE_PERIOD
+      return Math.ceil(age)
+    } else
+      return false
+  } else
+    return false
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  let count = 0
+  arr.forEach(e => {
+    e.forEach(e => {
+      if (e == "^^") {
+        count += 1
+      }
+    })
+  })
+  return count
 };
