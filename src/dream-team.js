67b4a0ee91089e0401cd3b7e643437c12ec5f621
lordshtein <lordshtein@gmail.com>
2020-03-13 22:04:21 +0300

    feat: dream-team done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(str) {
+  if (!Array.isArray(str)) return false
+  let res = [];
+  str.forEach( (e) => {
+    if (typeof e == "string") {
+      for (let i=0; i<e.length;i++) {
+        if (e[i] != " ") {
+          res.push(e[i].toUpperCase())
+          break
+        }
+      }
+
+    } else return false
+
+  })
+ return res.sort().join("")
+}
