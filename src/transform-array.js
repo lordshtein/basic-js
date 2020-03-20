0ea57d43e94d242eb70be70731282c5b8a729b5b
lordshtein <lordshtein@gmail.com>
2020-03-20 17:56:43 +0300

    save changes

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,26 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw 'Not array';
+    let res = []
+    for( let i=0; i < arr.length;i++) {
+        if (arr[i]== "--discard-next") {
+            i++
+        } else if (arr[i]== "--discard-prev") {
+            if (arr[i-1] || arr[i-1] == 0) {
+                res.pop()
+            }
+        } else if (arr[i]== "--double-next") {
+            if (arr[i+1] || arr[i+1] == 0) {
+            res.push(arr[i+1]);
+            res.push(arr[i+1]);
+            i++}
+        } else if (arr[i]== "--double-prev") {
+            if (arr[i-1] || arr[i-1] == 0) {
+                res.push(arr[i-1])
+            }
+        } else {
+            res.push(arr[i])
+        }
+
+    }
+    return res
 };
