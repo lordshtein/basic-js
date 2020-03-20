b20d90bfe6de00d06746183e31310380828fd868
lordshtein <lordshtein@gmail.com>
2020-03-20 17:24:58 +0300

    fix: hanoi tower bug fix

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,6 @@
 module.exports = function calculateHanoi(disk, speed) {
     const answer = {};
     answer['turns']=Math.pow(2,disk)-1;
-    answer['seconds']=Math.pow(2,disk)-1/(speed/3600) // two times faster then taking "turn" for object
+    answer['seconds']=(Math.pow(2,disk)-1)/(speed/3600) // two times faster then taking "turn" for object
     return answer
 }
