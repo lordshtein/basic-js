4300f898afed3bdb4f2809b6e8ec383f983b6353
lordshtein <lordshtein@gmail.com>
2020-03-22 13:59:28 +0300

    feat: tarnsfor array done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,26 +1,26 @@
 module.exports = function transform(arr) {
     if (!Array.isArray(arr)) throw 'Not array';
+
     let res = []
     for( let i=0; i < arr.length;i++) {
         if (arr[i]== "--discard-next") {
             i++
         } else if (arr[i]== "--discard-prev") {
-            if (arr[i-1] || arr[i-1] == 0) {
+            if (arr[i-1] || arr[i-1] == 0 || Number.isNaN(arr[i-1])) {
                 res.pop()
             }
         } else if (arr[i]== "--double-next") {
-            if (arr[i+1] || arr[i+1] == 0) {
+            if (arr[i+1] || arr[i+1] == 0 || Number.isNaN(arr[i+1])) {
             res.push(arr[i+1]);
             res.push(arr[i+1]);
             i++}
         } else if (arr[i]== "--double-prev") {
-            if (arr[i-1] || arr[i-1] == 0) {
+            if (arr[i-1] || arr[i-1] == 0 || Number.isNaN(arr[i-1]))  {
                 res.push(arr[i-1])
             }
         } else {
             res.push(arr[i])
         }
-
     }
     return res
 };
