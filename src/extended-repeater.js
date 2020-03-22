3f25f75f1936a58a6c4b7d64bcf235762a41533d
lordshtein <lordshtein@gmail.com>
2020-03-22 17:37:30 +0300

    feat: done extender repaeter

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,20 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater( str , options) {
+    let sep = options["separator"] ? options["separator"] : "+";
+    let addSep = options["additionSeparator"] ? options["additionSeparator"] : "|";
+    let add = options["addition"] === undefined ? "" : options["addition"]
+    let repTime = options["repeatTimes"] ? options["repeatTimes"] : 1;
+    let addRepTime = options["additionRepeatTimes"] ? options["additionRepeatTimes"] : 1;
+    
+    let fullAdd = add;
+    let fullStr = str;
+    if (repTime == 1 && addRepTime == 1)
+    return str+add
+
+    fullAdd = ( add + addSep ).repeat(addRepTime - 1) + add
+   
+    fullStr = (str + fullAdd + sep).repeat(repTime - 1) + str + fullAdd
+
+    return fullStr
 };
-  
+ 
+
